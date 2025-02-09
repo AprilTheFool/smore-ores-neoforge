be62496ea3d04b61d27046266ec8268841e80432
AprilTheFool <pixll1324@gmail.com>
2025-02-09 22:43:29 +1000

    Add initial mod setup for "S'more Ores"

---
+++ src/main/java/online/aprl/smoreores/block/ModBlocks.java
@@ -0,0 +1,49 @@
+package online.aprl.smoreores.block;
+
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import online.aprl.smoreores.block.custom.MagicBlock;
+import online.aprl.smoreores.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS =
+            DeferredRegister.createBlocks("smoreores");
+
+    public static final DeferredBlock<Block> BAZINGIUM_BLOCK = registerBlock("bazingium_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+
+    public static final DeferredBlock<Block> BAZINGIUM_ORE = registerBlock("bazingium_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 4),
+                    BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+    public static final DeferredBlock<Block> BAZINGIUM_DEEPSLATE_ORE = registerBlock("bazingium_deepslate_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 6),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
+
+    public static final DeferredBlock<Block> MAGIC_BLOCK = registerBlock("magic_block",
+            () -> new MagicBlock(BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/online/aprl/smoreores/block/custom/MagicBlock.java
@@ -0,0 +1,56 @@
+package online.aprl.smoreores.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import online.aprl.smoreores.item.ModItems;
+
+public class MagicBlock extends Block {
+
+    public MagicBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+
+
+        level.playSound(player, pos, SoundEvents.AMETHYST_CLUSTER_PLACE, SoundSource.BLOCKS, 1f, 1f);
+        return  InteractionResult.SUCCESS;
+    }
+
+    // Check entities on top of the block using the stepOn method
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        if(entity instanceof ItemEntity itemEntity) {
+
+            // Check if the entity is a supported item and convert it
+            if(itemEntity.getItem().getItem() == ModItems.RAW_BAZINGIUM.get()) {
+                itemEntity.setItem( new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
+            }
+
+            if(itemEntity.getItem().getItem() == ModItems.BAZINGIUM.get()) {
+                itemEntity.setItem( new ItemStack(Items.EMERALD, itemEntity.getItem().getCount()));
+            }
+
+        }
+        // Check if the entity is a LivingEntity and apply damage
+        if(!entity.isSteppingCarefully() && entity instanceof LivingEntity livingEntity) {
+            livingEntity.hurt(level.damageSources().hotFloor(), 1.0F);
+        }
+
+        super.stepOn(level, pos, state, entity);
+    }
+}
